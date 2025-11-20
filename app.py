4ba24585a8cc6649ee17adffc037957af3bcb4fb
riteshraj1362 <riteshraj1362@gmail.com>
2025-11-20 12:06:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import joblib
 
+# Load trained model
 model = joblib.load("sleep_model.pkl")
 
 st.title("Smart Sleep Predictor")
@@ -12,11 +13,13 @@ stress_level = st.slider("Stress Level (0 - No Stress, 10 - High Stress)", 0, 10
 bmi = st.number_input("BMI Value", 10.0, 50.0, step=0.1)
 
 if st.button("Predict"):
-data = pd.DataFrame([{
-"Age": age,
-"Sleep Duration": sleep_duration,
-"Stress Level": stress_level,
-"BMI": bmi
-}])
-result = model.predict(data)[0]
-st.success(f"Prediction: {result}")
+    data = pd.DataFrame([{
+        "Age": age,
+        "Sleep Duration": sleep_duration,
+        "Stress Level": stress_level,
+        "BMI": bmi
+    }])
+    
+    result = model.predict(data)[0]
+    st.success(f"Prediction: {result}")
+
